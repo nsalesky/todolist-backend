ceddbb7ea41a7b37d7ccd529785bcb7d74047b02
Nick Salesky <nicksalesky@gmail.com>
2021-07-21 14:37:46 -0700

    Improved the structure a bit, made everything a part of the library instead of the binary

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate serde;
+
+use rocket::{Build, Request};
+
+mod database;
+mod models;
+mod routes;
+mod schema;
+mod auth;
+mod constants;
+mod services;
+
+#[catch(404)]
+fn not_found(request: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", request.uri())
+}
+
+/// Builds the Rocket instance to use.
+pub fn rocket() -> rocket::Rocket<Build> {
+    rocket::build()
+        .mount("/api",
+        routes![
+            routes::users::signup,
+            routes::users::login,
+            routes::users::get_user,
+        ])
+        .attach(database::PostgresDbConn::fairing())
+        .register("/api", catchers![not_found])
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
-#[macro_use]
-extern crate diesel;
-#[macro_use]
-extern crate rocket;
-#[macro_use]
-extern crate serde;
+use todo_backend;
 
-use rocket::{Build, Rocket, State};
-use rocket::serde::json::{Json, Value};
-use rocket_sync_db_pools::Connection;
-use serde_json::json;
-
-use crate::database::PostgresDbConn;
-
-mod database;
-mod models;
-mod routes;
-mod schema;
-mod auth;
-mod constants;
-mod services;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello world!"
-}
-
-#[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build()
-        .mount("/", routes![index, routes::signup, routes::login, routes::get_user])
-        .attach(PostgresDbConn::fairing())
+#[rocket::main]
+async fn main() {
+    todo_backend::rocket()
+        .launch()
+        .await;
 }
